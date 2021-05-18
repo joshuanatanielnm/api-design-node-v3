e745627413249806547a0b7f9f344926f6cec9e2
Joshua Nathaniel M <clsjoshua@gmail.com>
2021-05-18 14:20:29 +0700

    progress: Controllers & Models Done

--- src/utils/crud.js
+++ src/utils/crud.js
@@ -1,12 +1,86 @@
-export const getOne = model => async (req, res) => {}
+import { set } from 'lodash'
 
-export const getMany = model => async (req, res) => {}
+export const getOne = model => async (req, res) => {
+  try {
+    const id = req.params.id
+    const userId = req.user._id
+    const doc = await model.findOne({ _id: id, createdBy: userId }).exec()
+    if (!doc) {
+      return res.status(400).end()
+    }
+    return res.status(200).json({ data: doc })
+  } catch (error) {
+    console.log(error)
+    res.status(400).end()
+  }
+}
+export const getMany = model => async (req, res) => {
+  try {
+    const docs = await model.find({ createdBy: req.user._id }).exec()
+    res.status(200).json({ data: docs })
+  } catch (error) {
+    console.log(error)
+    res.status(400).end()
+  }
+}
 
-export const createOne = model => async (req, res) => {}
+export const createOne = model => async (req, res) => {
+  try {
+    const doc = await model.create({ ...req.body, createdBy: req.user._id })
 
-export const updateOne = model => async (req, res) => {}
+    if (!doc) {
+      return res.status(400).end()
+    }
 
-export const removeOne = model => async (req, res) => {}
+    res.status(201).json({ data: doc })
+  } catch (error) {
+    console.log(error)
+    res.status(400).end()
+  }
+}
+
+export const updateOne = model => async (req, res) => {
+  try {
+    const updatedDoc = await model
+      .findOneAndUpdate(
+        {
+          createdBy: req.user._id,
+          _id: req.params.id
+        },
+        req.body,
+        { new: true }
+      )
+      .lean()
+      .exec()
+
+    if (!updatedDoc) {
+      return res.status(400).end()
+    }
+
+    res.status(200).json({ data: updatedDoc })
+  } catch (e) {
+    console.error(e)
+    res.status(400).end()
+  }
+}
+
+export const removeOne = model => async (req, res) => {
+  try {
+    const removed = await model.findOneAndRemove({
+      createdBy: req.user._id,
+      _id: req.params.id
+    })
+
+    if (!removed) {
+      return res.status(400).end()
+    }
+
+    return res.status(200).json({ data: removed })
+  } catch (e) {
+    console.error(e)
+    res.status(400).end()
+  }
+}
 
 export const crudControllers = model => ({
   removeOne: removeOne(model),
