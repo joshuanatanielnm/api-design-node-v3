37b10e4d9b5dbd8cebbd7b2d002d022bffca8a87
Joshua Nathaniel M <clsjoshua@gmail.com>
2021-05-18 09:26:23 +0700

    progress: crud documents

--- src/resources/item/item.controllers.js
+++ src/resources/item/item.controllers.js
@@ -1,4 +1,32 @@
 import { crudControllers } from '../../utils/crud'
 import { Item } from './item.model'
+import mongoose from 'mongoose'
+import { connect } from '../../utils/db'
 
-export default crudControllers(Item)
+const run = async () => {
+  await connect('mongodb://localhost:27017/api-test')
+
+  const item = await Item.create({
+    name: 'Clean Up',
+    createdBy: mongoose.Types.ObjectId(),
+    list: mongoose.Types.ObjectId()
+  })
+
+  const update = await Item.findByIdAndUpdate(
+    item._id,
+    { name: 'eat' },
+    { new: true }
+  ).exec()
+
+  console.log(update)
+}
+
+run()
+
+/**
+ * GET / read many
+ * GET/:id read one
+ * POST create one
+ * PUT update one
+ * Delete/:id delete one
+ */
